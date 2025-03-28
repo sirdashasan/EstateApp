f76fbc1b6acd29106089d47c6db55d749803701c
sirdashasan <sirdashasan@hotmail.com>
2025-03-28 10:38:22 +0300

    Add House model

---
+++ src/main/java/com/hasan/estateapp/model/House.java
@@ -0,0 +1,31 @@
+package com.hasan.estateapp.model;
+
+public class House {
+    private int price;
+    private int squareMeters;
+    private int numberOfRooms;
+    private int numberOfLivingRooms;
+
+    public House(int price, int squareMeters, int numberOfRooms, int numberOfLivingRooms) {
+        this.price = price;
+        this.squareMeters = squareMeters;
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfLivingRooms = numberOfLivingRooms;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getSquareMeters() {
+        return squareMeters;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getNumberOfLivingRooms() {
+        return numberOfLivingRooms;
+    }
+}
